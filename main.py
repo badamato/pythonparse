bc53472134b49927545ad3dd0ab7d594f14a10e8
Beth <beth.damato@coxautoinc.com>
2019-12-20 13:13:26 -0500

    adds lowercase check to argv[1]

--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 import sys
 import csv
 
-with open("customer_pages.csv", 'r', encoding='utf-8-sig') as file:
-    datafile = csv.DictReader(file)
+with open('customer_pages.csv', 'r', encoding='utf-8-sig') as file:
+  datafile = csv.DictReader(file)
 
-    for row in datafile:
-      if sys.argv[1] == row['Customer ID']:
-        print(row['Page ID'], end=' ')
-    print('\n')
+  for row in datafile:
+    print(row)
+  #   if sys.argv[1].upper() == row['Customer ID']:
+  #     print(row['Page ID'], end=' ')
+  # print('\n')
